f28f6cf255e87af1519ef88e39e5aac4d1aa84ac
Vinoth <ms23vinothp@gmail.com>
2026-07-07 11:49:43 +0530

    Updated Java and JavaScript files

---
+++ javaprograms/TwoSum.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class TwoSum
+{
+    public ststaic void main(String args[])
+    {
+        Scanner sc=new Scamnner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+
+        for(int i=0;i<n;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+
+    }
+}
